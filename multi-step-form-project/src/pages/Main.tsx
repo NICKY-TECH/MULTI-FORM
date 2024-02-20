88e7d654eff25c664476a59b0ec09dc594483971
NICKY-TECH <tinaonyis@gmail.com>
2024-02-20 10:23:32 +0100

    fix thank you page

--- multi-step-form-project/src/pages/Main.tsx
+++ multi-step-form-project/src/pages/Main.tsx
@@ -1,13 +1,10 @@
 import "../styles/destination.css";
 import { SideBar,Body } from "..";
-import { useAppSelector } from "../hooks/typedRedux";
 function Main() {
-  const pageNumberValue = useAppSelector((state)=> state.pageNumber.value);
   return <main>
 <div className="main-content">
-{
-  pageNumberValue===5?"":<SideBar/>
-}
+<SideBar/>
+
    <Body/>
 </div>
   </main>;
